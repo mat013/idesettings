237d898fc7924f84071d364340800f042866750b
Meang Akira Tanaka <meang.tnk@gmail.com>
2020-05-30 08:33:32 +0200

    Added examples on how to do foreach

--- bash/scripts/handle_eachfile.sh
+++ bash/scripts/handle_eachfile.sh
@@ -1,1 +1,14 @@
-xargs -0 echo < <(tr \\n \\0 <m.txt)
+# creates a file for each line in m.txt
+xargs -0 mkdir < <(tr \\n \\0 <m.txt)
+
+
+ls -d */ | sed 's/.$//' | xargs -0 mkdir
+
+# take each item in a file a.txt and execute
+xargs -a output.txt -0 ./m
+
+# take each item in output.txt and execute m with $line as argument
+cat output.txt | while read line; do ./m $line; done
+
+# get each item on each line and remove the trailing /
+ls -d */ | sed 's/.$//' > output.txt
