fad714739ced04b859f1c5eb84d6654afa26cd34
Meang Akira Tanaka <meang.tnk@gmail.com>
2020-06-21 20:15:37 +0200

    Adding removal of orphan branches in git

---
+++ bash/scripts/git_remove_orphan_branches.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Inspired by https://chrisseroka.wordpress.com/2017/12/07/cleaning-up-git-branches/
+
+echo fetching all
+git fetch --all --prune
+
+echo Fetching branch with missing remotes
+git branch -vv | grep ": gone\]" > .to-remove && vim .to-remove
+cat .to-remove
+
+read -p 'Continue? Yes if you want to continue: '
+
+if [[ $REPLY == 'yes' ]];
+then
+  grep ".*" .to-remove | cut -c 3- | awk '{print $1}' | xargs git branch -D
+fi
